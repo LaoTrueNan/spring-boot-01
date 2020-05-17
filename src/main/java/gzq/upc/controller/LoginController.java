6fbd896b84abfbffd32ec7537eea890f34cfeac1
LaoTrueNan <leonard.qiang@gmail.com>
2020-05-17 23:16:10 +0800

    登录controller

--- src/main/java/gzq/upc/controller/LoginController.java
+++ src/main/java/gzq/upc/controller/LoginController.java
@@ -2,32 +2,75 @@ package gzq.upc.controller;
 
 import gzq.upc.dataobject.SellerInfo;
 import gzq.upc.form.SellerForm;
+import gzq.upc.repository.SellerInfoRepository;
+import gzq.upc.repository.SellerRepository;
 import gzq.upc.service.LoginService;
+import gzq.upc.utils.IntUtil;
+import gzq.upc.utils.KeyUtil;
+import gzq.upc.utils.gzqCookie;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/welcome")
 public class LoginController {
-    @ResponseBody
+
+    @Autowired
+    private SellerInfoRepository repository;
+
+    @Autowired
+    private SellerRepository sellerRepository;
+
+    @Autowired
+    private LoginService loginService;
+
     @PostMapping("/login")
-    public String login(@Valid SellerForm form,BindingResult bindingResult){
-        return "<center><h1>"+form.getUsername()+"\t"+form.getPassword()+"</h1></center>";
+    public ModelAndView login(@Valid SellerForm form, BindingResult bindingResult, Map<String,Object> map, HttpServletResponse response){
+        String result = loginService.check(form.getUsername(),form.getPassword());
+        map.put("msg",result);
+        if(result.equals("success")) {
+            map.put("url", "/seller/sellerInfoPage?username="+form.getUsername());
+            gzqCookie.setCookie(response,"username",form.getUsername());
+            return new ModelAndView("common/success",map);
+        }else{
+            map.put("url","/");
+            return new ModelAndView("common/error",map);
+        }
     }
 
 
-//    public ModelAndView(@Valid SellerForm form,BindingResult bindingResult,Map<String,Object> map){
-//
-//
-//        return new ModelAndView("/seller/");
-//    }
+    @PostMapping("/regi")
+    public ModelAndView register(@Valid SellerForm form,BindingResult bindingResult,Map<String,Object> map){
+
+        String result = loginService.regi(form.getUsername(),form.getOpenid());
+        map.put("msg",result);
+        if(result.equals("Success")) {
+            SellerInfo sellerInfo = new SellerInfo();
+            form.setId(IntUtil.genUniqueKey());
+            BeanUtils.copyProperties(form, sellerInfo);
+            repository.save(sellerInfo);
+            map.put("url", "/");
+            return new ModelAndView("common/success",map);
+        }else{
+            map.put("url","/regi.html");
+            return new ModelAndView("common/error",map);
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(){
+        return "index";
+    }
 }
